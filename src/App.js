ba7651427e1da3c6edbdc0a716b1758d348a1cd1
robonexx <robertwagar@gmail.com>
2021-11-20 22:40:13 +0100

    Added a nav and a hover effect on the links

--- src/App.js
+++ src/App.js
@@ -1,45 +1,99 @@
-import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-import BoxesPage from "./BoxesPage";
-import Gsap_Svg from "./Gsap_Svg";
-import TextPage from "./TextPage";
-import './App.css'
+import React, { useRef, useEffect, forwardRef } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { gsap } from 'gsap';
+import BoxesPage from './BoxesPage';
+import Gsap_Svg from './Gsap_Svg';
+import TextPage from './TextPage';
+import './App.css';
 
 export default function App() {
+  const btnRef = useRef();
+
+  const onEnter = ({ currentTarget }) => {
+    gsap.to(currentTarget, {
+      backgroundColor: '#e77614',
+      scale: 1.1,
+      ease: 'Power2.easeOut',
+      duration: 0.4,
+    });
+  };
+
+  const onLeave = ({ currentTarget }) => {
+    gsap.to(currentTarget, {
+      backgroundColor: 'transparent',
+      scale: 1,
+      ease: 'Power2.easeIn',
+      duration: 0.4,
+    });
+  };
+
+  /* const onEnter = (e) => {
+    gsap.from(e, { yPercent: 100, backgroundColor: 'transparent' });
+    gsap.to(e, {yPercent: 0, scale: 1.1,delay: 0.1, ease: "power2.out", backgroundColor: '#e77614'})
+  };
+
+  const onLeave = (e) => {
+    gsap.from('.bg', {yPercent: 0, scale: 1.1, backgroundColor: '#e77614'});
+    gsap.to('.bg', {
+      yPercent: 100, backgroundColor: 'transparent', ease: "power2.out"
+    });
+    clearImmediate()
+  }; */
   return (
     <Router>
       <div>
         <nav>
-         
-              <Link to="/">Home</Link>
-            
-              <Link to="/svg">Svg</Link>
-            
-              <Link to="/boxes">Boxes</Link>
-            
-              <Link to="/text">Text</Link>
-            
+          <li className='btn'>
+            <Link
+              to='/'
+              ref={btnRef}
+              onMouseEnter={onEnter}
+              onMouseLeave={onLeave}
+            >
+              Home
+            </Link>
+          </li>
+          <li className='btn'>
+            <Link
+              to='/svg'
+              ref={btnRef}
+              onMouseEnter={onEnter}
+              onMouseLeave={onLeave}
+            >
+              Svg
+            </Link>
+          </li>
+          <li className='btn'>
+            <Link
+              to='/boxes'
+              ref={btnRef}
+              onMouseEnter={onEnter}
+              onMouseLeave={onLeave}
+            >
+              Boxes
+            </Link>
+          </li>
+
+          {/*    
+          <Link to="/text" className="btn" ref={btnRef}
+          onMouseEnter={onEnter}
+          onMouseLeave={onLeave}>Text</Link> */}
         </nav>
 
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
         <Switch>
-          <Route path="/boxes">
+          <Route path='/boxes'>
             <BoxesPage />
           </Route>
-          <Route path="/text">
+          <Route path='/text'>
             <TextPage />
           </Route>
-          <Route path="/svg">
+          <Route path='/svg'>
             <Gsap_Svg />
           </Route>
 
-          <Route path="/">
+          <Route path='/'>
             <Home />
           </Route>
         </Switch>
@@ -50,7 +104,7 @@ export default function App() {
 
 function Home() {
   return (
-    <div className="home">
+    <div className='home'>
       <h1>HOME</h1>
       <p>Learning Gsap and Svg manipulation</p>
     </div>
